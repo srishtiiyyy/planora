5c0a129ddc8f1a9d0c0e40d5ad83a41480120a2e
Srishti <srishtisingh910@gmail.com>
2025-08-11 13:39:42 +0530

    popularcitylist

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import { PopularCityList } from "./_components/PopularCityList";
 
 export default function Home() {
   return (
     <div >
       
       <Hero/>
+      <PopularCityList />
     </div>
   )
 }
