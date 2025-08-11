74267545f2bfe5f7aea58be4f6b3f03d03af8c90
Srishti <srishtisingh910@gmail.com>
2025-08-11 12:32:01 +0530

    importing hero in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div >
-      <h2></h2>
       
+      <Hero/>
     </div>
   )
 }
