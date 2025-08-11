f6177edb8de559799540ef5e0800d6f7ddb21005
Srishti <srishtisingh910@gmail.com>
2025-08-11 12:31:33 +0530

    landing page

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="mt-24 flex flex-col items-center text-center">
+      {/* First div */}
+      <div></div>
+
+      {/* content goes here */}
+      <div>
+        <h1 className="text-xl md:text-5xl font-bold">
+          Hey, I'm your personal <span className="text-primary">TRIP PLANNER</span>
+        </h1>
+        
+        <p className="mt-4 text-lg md:text-2xl font-medium">
+          From idea to itinerary — flights, hotels, and stays in seconds.
+        </p>
+      </div>
+
+      {/* input box */}
+      {/* suggestion list */}
+      {/* video section */}
+    </div>
+  )
+}
+
+export default Hero
+
